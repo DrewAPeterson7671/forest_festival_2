03d5b0c68d209172f32613fd891b0a53042e6982
Student <example@example.com>
2019-12-18 09:23:17 -0800

    Created directories and initial files, passed first three tests.

--- lib/festival.rb
+++ lib/festival.rb
@@ -1,9 +1,32 @@
+class Stage
+    attr_accessor :name
+    attr_reader :id
 
-    class Stage
-      def initialize
-      end
-      def self.all()
-        
-      end
-    end
-    
+  @@stages = {}
+  @@total_rows = 0
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id) || @@total_rows += 1
+  end
+
+  def self.all()
+    @@stages.values()
+  end
+
+  def save
+    @@stages[self.id] = Stage.new({ :name => self.name, :id => self.id})
+  end
+
+  def ==(stage_to_compare)
+    self.name() == stage_to_compare.name()
+  end
+
+  def self.clear
+    @@stages = {}
+    @@total_rows = 0
+  end
+
+
+
+end
--- spec/festival_spec.rb
+++ spec/festival_spec.rb
@@ -1,13 +1,34 @@
+require('rspec')
+require('festival')
 
-    require('rspec')
-    require('festival')
+describe(Stage) do
 
-    describe(Stage) do
-    describe('#self.all') do
-      it('test') do
-        test = Stage.new()
-        expect(test.self.all()).to(eq('test'))
-      end
+  before(:each) do
+    Stage.clear
+  end
+
+  describe('.all') do
+    it('returns an empty array when no stages') do
+      expect(Stage.all).to(eq([]))
     end
   end
-  
+  describe('#save') do
+    it('saves a new stage') do
+      stage = Stage.new({:name => "stage 1", :id => nil})
+      stage.save()
+      stage2 = Stage.new({:name => "stage 2", :id => nil})
+      stage2.save()
+      expect(Stage.all).to(eq([stage, stage2]))
+    end
+  end
+  describe('#clear') do
+    it('clears all stages') do
+      stage = Stage.new({:name => "stage 1", :id => nil})
+      stage.save()
+      stage2 = Stage.new({:name => "stage 2", :id => nil})
+      stage2.save()
+      Stage.clear
+      expect(Stage.all).to(eq([]))
+    end
+  end
+end
